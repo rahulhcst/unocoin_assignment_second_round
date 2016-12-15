ac187f2726f92740c72bf8594c20e37a489c1597
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-15 08:49:07 +0530

    Addded in command student

--- app/Console/Commands/Student.php
+++ app/Console/Commands/Student.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Console\Command;
 
 class Student extends Command
@@ -39,7 +40,61 @@ class Student extends Command
     {
         $studentDetails = [];
         $count  = 0;
+        $students = [];
+        $subjects = [];
+        $count = $this->ask('Enter number of students', 0);
+        if ($count)
+        {
+            for ($i = 0; $i < $count; $i++)
+            {
+                $input = $this->ask('Enter name of STUDENT or LEAVE BLANK and hit return to exit', false);          //Reading input students name entered by user
+                if ($input)
+                    array_push($students, $input);
+                else
+                    break;
+            }
+        }
+        die;
+        echo "------------------------------------------------------------\r\n\r\n";
 
-        $this->ask();
+        $count = $this->ask('Enter number of students', 0);
+        if($count)
+        {
+            for ($i = 0; $i < $count; $i++)
+            {
+                $input = $this->ask('Enter name of SUBJECT or LEAVE BLANK and hit return to exit', false);          //Reading input students name entered by user
+                if ($input)
+                    array_push($subjects, $input);
+                else
+                    break;
+            }
+        }
+
+        echo "------------------------------------------------------------\r\n\r\n";
+
+        /*
+         * For taking marks of each student for each subject
+         */
+        foreach ($students as $student) {                           //Iterating over students array
+            $details = [];                                          //Array for storing student's detail
+            echo "Enter marks for student $student:\r\n";
+            $details['name'] = $student;                            //Assigning student name
+            $details['marks'] = [];                                 //Array for storing student's marks for each subject
+            $details['total'] = 0;                                  //For storing totral marks, initialising with 0
+            foreach ($subjects as $subject) {
+                $marks = $this->ask('Enter marks of '.$subject, false);         //Input for taking marks from user of each subject
+                if (!$marks)
+                    $marks = 0;                                                 //If marks is not enterd assuming 0
+                array_push($details['marks'], $marks);                          //Pushing marks into $details['marks'] array
+                $details['total'] += $marks;                                    //Taking sum of the marks
+            }
+            array_push($studentDetails, $details);                              //Pushin details into array $studentDetails
+        }
+
+        //$this->as
+
+        $studentObj = new StudentController($studentDetails, $subjects);        //Instantiating class StudentController and passing array studentDetails, $subjects
+        $studentObj->generateResult();                                                 //Calling function execute
+
     }
 }
